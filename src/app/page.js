a8924c43b0c1153af5788c63d5b2fd1f4f1a7bc0
Dussan-Mamani <dussanmamani0@gmail.com>
2024-01-26 10:05:45 +0100

    Asignación de Pokemon

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Pokemon from "./components/pokemon";
 
 export default function Home() {
   return (
-    <div></div>  
+    <div>
+      <Pokemon/>
+    </div>  
     );
 }
